7f771047a121924bedecfbb1af5d9d18a3897605
macbook-quentin-github <q.mberi@wesyn.fr>
2025-01-10 16:24:35 +0100

    correction pour passer sur java 11

--- src/test/java/guru/springframework/SpringCoreDevOpsApplicationTests.java
+++ src/test/java/guru/springframework/SpringCoreDevOpsApplicationTests.java
@@ -1,5 +1,6 @@
 package guru.springframework;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
